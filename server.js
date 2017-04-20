ffedd511bf4f438e867915736edf27816e773cb7
Gina Fitzgerald <g24fitzgerald@gmail.com>
2017-04-20 16:04:06 -0500

    modifed for heroku deployment

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 //lets us use express
 var express = require('express');
-
+//for heroku to pass us the port or run on localhost:3000
+const PORT = process.env.PORT || 3000;
 //create our app
 var app = express();
-
+//express middleware, redirect https to an http address
+app.use(function(req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http'){
+    next(); //lets req process as normal
+  } else {
+    res.redirect('http://' + req.hostname + req.url);//redirect https to http
+  }
+});
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function(){
+  console.log('Express server is up on port ' + PORT);
 });
